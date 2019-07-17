7632623a076343ce47d4bc8d56cf51e8aee95880
ImsirovicAjdin <ajdika@live.com>
2019-07-17 16:43:50 +0200

    Ad app-child.component.ts

---
+++ src/app/appchild/app-child.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output, EventEmitter } from '@angular/core'
+
+@Component({
+  selector: 'app-child',
+  template: `
+    <div>
+      <div>
+        child
+      </div>
+      <button (click)=sendDataToParent()">
+        Send data to parent
+      </button>
+    </div>
+  `
+})
+export class AppChildComponent {
+  @Output() dataFromChild = new EventEmitter;
+
+  sendDataToParent() {
+    return this.dataFromChild.emit('data from child');
+  }
+}
